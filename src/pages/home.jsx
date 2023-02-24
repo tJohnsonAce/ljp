988497663ed5b8dc5c2bec8483fff9fef93048ac
Trevor Johnson <75464105+tJohnsonAce@users.noreply.github.com>
2023-02-24 14:26:41 -0500

    testing pages for moblie layout fix

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+
+import Gallery from 'components/Gallery'
+import Hero from 'components/Hero'
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      <Hero heading='Contact' message='Complete the form below to get in touch for details.'/>
+      <Gallery />
+    </div>
+  )
+}
+
+export default contact
